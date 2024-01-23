9db1f27a39f9d2f6f90875bf58f9671c31f9c5a3
Sachin6s <141307978+Sachin6s@users.noreply.github.com>
2024-01-23 09:55:18 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -3,4 +3,12 @@
 int main () {
     int a,b,sum;
     
+    printf("Enter the first number");
+    scanf("%d",&a);
+    printf("Enter the second number");
+    scanf("%d",&b);
+
+    sum = a+b;
+    printf("The sum of %d and %d is %d/n",a,b,sum);
+    return 0;
 }
