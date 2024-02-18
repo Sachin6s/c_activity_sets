0a7e405792d0d33091021774a855baf03128aef4
Sachin6s <141307978+Sachin6s@users.noreply.github.com>
2024-02-18 11:59:47 +0530

    test

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
-
-int input_n() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    return n;
+#include<stdio.h>
+int input_n();
+int sum_n_nos(int n,int sum);
+void output(int n, int sum);
+int main()
+{
+    int n,sum=0;
+    n=input_n();
+    sum=sum_n_nos(n,sum);
+    output(n,sum);
+    return 0;
 }
-
-int sum_n_nos(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum += i;
+int input_n()
+{
+    int a;
+    printf("Enter the value of n:\n");
+    scanf("%d",&a);
+    return a;
+}
+int sum_n_nos(int n,int sum)
+{
+    int i;
+   for(i=1;i<=n;i++)
+    {
+        sum=sum+i;    
     }
     return sum;
 }
-
-void output(int n, int sum) {
-    printf("The sum of natural numbers from 1 to %d is: %d\n", n, sum);
-}
-
-int main() {
-    int n, sum;
-    
-    n = input_n(); 
-    sum = sum_n_nos(n); 
-    output(n, sum); 
-
-    return 0;
+void output(int n, int sum)
+{
+    printf("The sum of %d is %d\n",n,sum);
 }
