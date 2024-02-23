fe9630afef127cdf6f070ac932ab480210d0a1f8
Sachin6s <141307978+Sachin6s@users.noreply.github.com>
2024-02-23 17:06:34 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -3,8 +3,8 @@
 int main() {
     int a,b,sum;
     printf("Enter a and b\n");
-    scanf("%d %d",a,b);
+    scanf("%d %d",&a,&b);
     sum=a+b;
-    printf("the sum of %d and %d is %d\n",a,b,sum);
+    printf("the sum is %d\n",sum);
     return 0;
 }
