7c7fa03859ac10e3563b909b1b20f66a1cdeede0
Sachin6s <141307978+Sachin6s@users.noreply.github.com>
2024-02-23 17:02:13 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {
     int a,b,sum;
-    printf("Enter a and b");
+    printf("Enter a and b\n");
     scanf("%d %d",a,b);
     sum=a+b;
     printf("the sum of %d and %d is %d\n",a,b,sum);
