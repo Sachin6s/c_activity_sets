2a9b74120f39381d210c743bcbdec63ce856e0f2
Sachin6s <141307978+Sachin6s@users.noreply.github.com>
2024-02-28 17:14:35 +0530

    test

--- set01/problem08.c
+++ set01/problem08.c
@@ -16,14 +16,14 @@ int main()
 int input_array_size()
 {
     int n;
-    printf("Enter  the size of the array:\n");
+    printf("Enter the size of  array:\n");
     scanf("%d",&n);
     return n;
 }
 void input_array(int n, int a[n])
 {
     int i;
-    printf("Enter the vlues of the array:\n");
+    printf("Enter the values of the array:\n");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -40,5 +40,5 @@ int sum_n_array(int n, int a[n],int sum)
 }
 void output(int n,int sum)
 {
-    printf("The sum of the array of size %d is %d\n",n,sum);
+    printf("The sum of  array of size %d is %d\n",n,sum);
 }
